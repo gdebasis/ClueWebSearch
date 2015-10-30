36644900a471009721953c33b5ff984bf6664695
ttomilla <ttomilla@bitbucket.org>
2015-10-30 14:05:10 +0000

    commit milla

--- src/java/indexer/ClueWebDocIndexer.java
+++ src/java/indexer/ClueWebDocIndexer.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -5,7 +6,7 @@
  */
 
 package indexer;
-
+//comment milla
 import java.io.*;
 import java.util.*;
 import java.util.zip.GZIPInputStream;
