b98548f82817e5ac00dc9e02aa00ebca1eb45c19
ttomilla <tto_cam@hotmail.com>
2017-02-11 18:07:02 +0100

    pull request to fix the "selection" functionality
    * rename for win problem
    
    * previous version restored
    
    * patch applied

--- src/java/servlets/SearchServlet.java
+++ src/java/servlets/SearchServlet.java
@@ -9,19 +9,23 @@ package servlets;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.StringReader;
 import java.util.HashMap;
-import java.util.*;
+import java.util.Properties;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonException;
+import javax.json.JsonNumber;
+import javax.json.JsonReader;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import searcher.WT10GRetriever;
 import javax.servlet.http.HttpSession;
-import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
 import searcher.ClueWebRetriever;
-import searcher.MockRetriever;
 /**
  *
  * @author dganguly
@@ -53,34 +57,31 @@ public class SearchServlet extends HttpServlet {
     
     /* Return the snapshot of the current page */
     String getPageViewOfResultList(HttpServletRequest request,
-            String query, String indexNumStr, String pageNumberStr,HttpSession session)
+            String query, String indexNumStr, String pageNumberStr, int[] selection)
             throws Exception {
-        
+
         int indexNumber = -1;
 
-		if(indexNumStr != null)
-			indexNumber = Integer.parseInt(indexNumStr);
+        if (indexNumStr != null) {
+            indexNumber = Integer.parseInt(indexNumStr);
+        }
         int pageNumber = Integer.parseInt(pageNumberStr);
-        
-        HashMap<Integer, Integer> hitOrder = retriever.chooseIndexHitOrder(session, query);        
-        String key = null;
-		if (indexNumStr != null)
-        	key = query + "." + indexNumStr;
-		else
-			key = query;
-        System.out.println("key : :: "+key);
-		TopDocs topDocs = (TopDocs)session.getAttribute(key);
-        if (topDocs != null) {
-            ScoreDoc[] scoreDocs = topDocs.scoreDocs;
-            System.out.println("Entered in session");
-            return retriever.constructJSONForRetrievedSet(hitOrder, query, scoreDocs, indexNumber, pageNumber);
-        }       
-        
-			
-        topDocs = retriever.retrieve(hitOrder, query, indexNumber, pageNumber);
+        HttpSession session = request.getSession();
 
-        session.setAttribute(key, topDocs);
-        return retriever.constructJSONForRetrievedSet(hitOrder, query, topDocs.scoreDocs, indexNumber, pageNumber);
+        HashMap<Integer, Integer> hitOrder = retriever.chooseIndexHitOrder(session, query);
+        String key;
+        if (indexNumStr != null) {
+            key = query + "." + indexNumStr;
+        } else {
+            key = query;
+        }
+        TopDocs topDocs = (TopDocs) session.getAttribute(key);
+        if (topDocs == null) {
+            topDocs = retriever.retrieve(hitOrder, query, indexNumber, pageNumber);
+            session.setAttribute(key, topDocs);
+        }
+
+        return retriever.constructJSONForRetrievedSet(hitOrder, query, topDocs.scoreDocs, indexNumber, pageNumber, selection);
     }
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -95,58 +96,38 @@ public class SearchServlet extends HttpServlet {
         throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-		HttpSession session = request.getSession();
-        System.out.println((String)session.getId());
         String html = null;
         try {
             String queryStr = request.getParameter("query");
             System.out.println("query = |" + queryStr + "|");
-            String pageNum = request.getParameter("pageNum");
+            String pageNum = request.getParameter("page");
             System.out.println("page = |" + pageNum + "|");
             String indexNum = request.getParameter("index");
             System.out.println("index = |" + indexNum + "|");
-            
             String selection = request.getParameter("selection");
-            ArrayList<Integer> selectionArray = new ArrayList<>();
+            System.out.println("selection = |" + selection + "|");
 
-	    if(selection != null)
-            {
-		selection = selection.replace("[", "");
-            	selection = selection.replace("]", "");
-            	String[]selectionAr = selection.split(",");
-
-		for(int i = 0; i <  selectionAr.length;i++)
-                 	selectionArray.add(Integer.parseInt(selectionAr[i]));
-            }
-       	   if (pageNum == null && selection == null) { // no pagination workflow
-           {
+            if (pageNum == null && selection == null) { // no pagination workflow
                 html = retriever.retrieve(queryStr);
-		out.println(html);}
-           }
-           else { // pagination workflow
-                System.out.println("entered...........");
-		if(selection != null)
-		{
-             		TopDocs topDocs = (TopDocs)session.getAttribute(queryStr);
-             
-             		if(topDocs == null)
-             		{
-               			out.print("No query found");
-             		}		
-             		else
-             		{	    
-				System.out.println("session found...........");
-				MockRetriever ret = new MockRetriever();
-				out.print(ret.constructJSONForRetrievedSet(topDocs.scoreDocs, selectionArray));
-			}
-		}
-                else
-               {
-		 	html = getPageViewOfResultList(request, queryStr, indexNum, pageNum,session);
-			out.println(html);
-		}
             }
-            
+            else { // pagination workflow
+                int[] selectionList = null;
+                if (selection != null) {
+                    try (JsonReader reader = Json.createReader(new StringReader(selection))) {
+                        JsonArray array = reader.readArray(); //JsonParsingException? seems to throw JsonException 
+                        selectionList = new int[array.size()];
+                        int i = 0;
+                        for (JsonNumber number : array.getValuesAs(JsonNumber.class)) { //ClassCastException
+                            selectionList[i++] = number.intValueExact(); //ArithmeticException
+                        }
+                    } 
+                    catch (JsonException | ClassCastException | ArithmeticException e) {    
+                        selectionList = null;
+                    }
+                }
+                html = getPageViewOfResultList(request, queryStr, indexNum, pageNum, selectionList);
+            }            
+            out.println(html);                
             out.close();
         }
         catch (Exception ex) {
@@ -194,4 +175,3 @@ public class SearchServlet extends HttpServlet {
     }// </editor-fold>
 
 }
-
